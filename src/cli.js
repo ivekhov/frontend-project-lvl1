130918c8ea342230a53326bfc3c3afc4eed6820a
ivekhov <ivekhov@gmail.com>
2021-06-18 21:44:12 +0300

    Remove old files: cli and brain-games.

--- src/cli.js
+++
@@ -1,8 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const hello = 'Hello, ';
-const userName = readlineSync.question('May I have your name?\n');
-
-console.log('Welcome to the Brain Games!');
-
-console.log(hello.concat(userName, '!'));
