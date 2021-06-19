1d0f917cf59cc49a389046a16f5d3d60b7efd347
ivekhov <ivekhov@gmail.com>
2021-06-19 09:21:22 +0300

    Revert "Remove old files: cli and brain-games."
    This reverts commit 130918c8ea342230a53326bfc3c3afc4eed6820a.

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const hello = 'Hello, ';
+const userName = readlineSync.question('May I have your name?\n');
+
+console.log('Welcome to the Brain Games!');
+
+console.log(hello.concat(userName, '!'));
